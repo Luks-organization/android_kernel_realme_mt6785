9650f8ae792dba3ff0c31ec79900c4455d78f145
Stim Luks <stimluks87@gmail.com>
2025-03-06 15:14:05 +0500

    UPSTREAM: zram: add stat to gather incompressible pages since zram set up Currently, zram supports the stat via /sys/block/zram/mm_stat to represent how many of incompressible pages are stored at the moment but it couldn't show how many times incompressible pages were wrote down since zram set up.  It's also good indication to see how zram is effective in the system.
    Link: https://lkml.kernel.org/r/20201130201907.1284910-1-minchan@kernel.org
    Signed-off-by: Minchan Kim <minchan@kernel.org>
    Reviewed-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Minchan Kim <minchan@google.com>
    (cherry picked from commit 194e28da1a0279ef6a106a5b621fd79c410432ef)
    Bug: 173746187
    Change-Id: I5c1febc7cd1fc0c9f05f826e5fcf34931516afd9

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -46,6 +46,9 @@ static int zram_major;
 static struct zram *zram_devices;
 static const char *default_compressor = "CONFIG_ZRAM_DEFAULT_COMP_ALGORITHM";
 
+static bool is_lzorle;
+static unsigned char lzo_marker[4] = {0x11, 0x00, 0x00};
+
 /* Module params (documentation at end) */
 static unsigned int num_devices = 1;
 /*
