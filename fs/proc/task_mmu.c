cbbd9a8d0b57daa8379104aca37a69c3a140d960
StimLuks87 <san10031987san@gmail.com>
2025-10-13 07:04:38 +0500

    ../fs/proc/task_mmu.c:401:1: warning: unused label 'bypass' [-Wunused-label]   401 | bypass:       | ^~~~~~~

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -398,7 +398,7 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma)
 	start = vma->vm_start;
 	end = vma->vm_end;
 	show_vma_header_prefix(m, start, end, flags, pgoff, dev, ino);
-bypass:
+
 	/*
 	 * Print the dentry name for named mappings, and a
 	 * special [heap] marker for the heap:
