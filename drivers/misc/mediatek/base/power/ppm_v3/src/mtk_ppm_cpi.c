a0692034c9439b6c12589b9d2226acb7f8e6a32d
StimLuks87 <san10031987san@gmail.com>
2025-10-14 02:01:42 +0500

    Revert "drivers:/ mediatek: ppm_v3: Use separate `perf_event_read_local` function"
    This reverts commit 1bc95cfecfd6812746caa60ec08d188e3511a92a.

--- drivers/misc/mediatek/base/power/ppm_v3/src/mtk_ppm_cpi.c
+++ drivers/misc/mediatek/base/power/ppm_v3/src/mtk_ppm_cpi.c
@@ -94,11 +94,6 @@ static struct perf_event_attr pmu_e8_event_attr = {
 static unsigned long long stall_val[4];
 static int stall_val_dbg;
 
-int ppm_perf_event_read_local(struct perf_event *ev, u64 *value)
-{
-	return perf_event_read_local(ev, value, NULL, NULL);
-}
-
 static unsigned int ppm_cpi_get_cpu_cycle_count(int cpu)
 {
 #if 1
@@ -121,7 +116,7 @@ static unsigned int ppm_cpi_get_cpu_cycle_count(int cpu)
 	unsigned int diff = 0;
 
 	if (event && event->state == PERF_EVENT_STATE_ACTIVE) {
-		ppm_perf_event_read_local(event, &new);
+		perf_event_read_local(event, &new);
 		if (new > old)
 			diff = (unsigned int)(new - old);
 
@@ -143,7 +138,7 @@ static unsigned int ppm_cpi_get_inst_count(int cpu)
 	unsigned int diff = 0;
 
 	if (event && event->state == PERF_EVENT_STATE_ACTIVE) {
-		ppm_perf_event_read_local(event, &new);
+		perf_event_read_local(event, &new);
 		if (new > old)
 			diff = (unsigned int)(new - old);
 
@@ -164,7 +159,7 @@ static unsigned long long ppm_cpi_get_pmu_e1_count(int cpu)
 	unsigned long long diff = 0;
 
 	if (event && event->state == PERF_EVENT_STATE_ACTIVE) {
-		ppm_perf_event_read_local(event, &new);
+		perf_event_read_local(event, &new);
 		if (new > old)
 			diff = new - old;
 
@@ -185,7 +180,7 @@ static unsigned long long ppm_cpi_get_pmu_e7_count(int cpu)
 	unsigned long long diff = 0;
 
 	if (event && event->state == PERF_EVENT_STATE_ACTIVE) {
-		ppm_perf_event_read_local(event, &new);
+		perf_event_read_local(event, &new);
 		if (new > old)
 			diff = new - old;
 
@@ -206,7 +201,7 @@ static unsigned long long ppm_cpi_get_pmu_e8_count(int cpu)
 	unsigned long long diff = 0;
 
 	if (event && event->state == PERF_EVENT_STATE_ACTIVE) {
-		ppm_perf_event_read_local(event, &new);
+		perf_event_read_local(event, &new);
 		if (new > old)
 			diff = new - old;
 
@@ -292,7 +287,7 @@ static void ppm_cpi_pmu_enable_locked(int cpu, int enable)
 #endif
 		if (i_event) {
 			perf_event_enable(i_event);
-			ppm_perf_event_read_local(i_event,
+			perf_event_read_local(i_event,
 					      &per_cpu(inst_count, cpu));
 		}
 		if (p1_event)
