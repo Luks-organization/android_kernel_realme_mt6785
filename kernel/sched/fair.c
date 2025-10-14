d8390e0c777ae85e7533238a118f1e9ead6692dd
StimLuks87 <san10031987san@gmail.com>
2025-10-14 16:55:42 +0500

    [ALPS07354828] sched: select idle_sbling when cpu overutilized [Detail] select idle_sbling when cpu overutilized
    MTK-Commit-Id: f9bd61239b132f57b9f13058fc7ce94721a82f93
    
    CR-Id: ALPS07354828
    Feature: Energy Aware Scheduling Plus (EAS+)
    Signed-off-by: Lixiong Zhou <lixiong.zhou@mediatek.com>
    Change-Id: Id5b6fdbbd0be6e0dba5f97af9551cfb74ed573e9
    (cherry-pick from 3fe64a08a179515dcaca753368d5a9d319640e72)

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7208,7 +7208,7 @@ static inline int find_idlest_cpu(struct sched_domain *sd, struct task_struct *p
 	int new_cpu = cpu;
 
 	if (!cpumask_intersects(sched_domain_span(sd), &p->cpus_allowed))
-		return prev_cpu;
+                return -1;
 
 	while (sd) {
 		struct sched_group *group;
