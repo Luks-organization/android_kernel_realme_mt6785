c9fa2f34d2854b86dbc0bfb0b7429991a430648f
StimLuks87 <san10031987san@gmail.com>
2025-10-29 12:41:42 +0500

    [ALPS04314047] DISP: fix build break when CONFIG_PM_AUTOSLEEP=n Fix the following buidl break when CONFIG_PM_AUTOSLEEP=n
    ../drivers/misc/mediatek/video/mt6761/videox/mtkfb.c:2840:13:
    error: unused function mtkfb_late_resume [-Werror,-Wunused-function]
    static void mtkfb_late_resume(void)
    ../drivers/misc/mediatek/video/mt6761/videox/mtkfb.c:2820:13:
    error: unused function mtkfb_early_suspend [-Werror,-Wunused-function]
    static void mtkfb_early_suspend(void)
    
    MTK-Commit-Id: 79cfef73dd31029a8f953153ec946ff3d388bdd6
    
    Change-Id: Id104ba411d1203f4c91c7baf47f92a6dab60335f
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>
    CR-Id: ALPS04314047
    Feature: [Module]Kernel Memory Management

--- drivers/misc/mediatek/video/mt6785/videox/mtkfb.c
+++ drivers/misc/mediatek/video/mt6785/videox/mtkfb.c
@@ -235,9 +235,10 @@ static int mtkfb_get_overlay_layer_info(
 #ifdef CONFIG_OF
 static int _parse_tag_videolfb(void);
 #endif
-
+#if defined(CONFIG_PM_AUTOSLEEP)
 static void mtkfb_late_resume(void);
 static void mtkfb_early_suspend(void);
+#endif
 
 void mtkfb_log_enable(int enable)
 {
@@ -2825,6 +2826,7 @@ void mtkfb_clear_lcm(void)
 {
 }
 
+#if defined(CONFIG_PM_AUTOSLEEP)
 static void mtkfb_early_suspend(void)
 {
 	int ret = 0;
@@ -2843,6 +2845,7 @@ static void mtkfb_early_suspend(void)
 
 	DISPMSG("%s-\n", __func__);
 }
+#endif
 
 /* PM resume */
 static int mtkfb_resume(struct platform_device *pdev)
@@ -2854,6 +2857,7 @@ static int mtkfb_resume(struct platform_device *pdev)
 	return 0;
 }
 
+#if defined(CONFIG_PM_AUTOSLEEP)
 static void mtkfb_late_resume(void)
 {
 	int ret = 0;
@@ -2873,9 +2877,11 @@ static void mtkfb_late_resume(void)
 	DISPMSG("%s-\n", __func__);
 
 }
+#endif
 
+/*---------------------------------------------------------------------------*/
 #ifdef CONFIG_PM
-
+/*---------------------------------------------------------------------------*/
 int mtkfb_pm_suspend(struct device *device)
 {
 	/* pr_debug("calling %s()\n", __func__); */
