3933bf2a2a5180f06247d0ada442bea35d86aba3
StimLuks87 <san10031987san@gmail.com>
2025-11-04 11:58:32 +0500

    Revert "kernel: signal: Mark do_freezer_trap as noinline for Thin LTO"
    This reverts commit 746af9b44f6436442b54d617aab8ba34121320f5.

--- kernel/signal.c
+++ kernel/signal.c
@@ -2200,7 +2200,7 @@ static void do_jobctl_trap(void)
  * Must be called with @current->sighand->siglock held,
  * which is always released before returning.
  */
-noinline static void do_freezer_trap(void)
+static void do_freezer_trap(void)
 	__releases(&current->sighand->siglock)
 {
 	/*
